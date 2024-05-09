428ba3a3c0b7d03a2eb6a4f2fa8aa03e5d9b8053
Jbunz <jacob652@aol.com>
2024-05-08 20:48:56 -0700

    added review page file

---
+++ src/ReviewPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ReviewPage() {
+  return (
+    <div className="ReviewPage">
+      <h1>Review Page</h1>
+      <p>This is the review page content.</p>
+    </div>
+  );
+}
+
+export default ReviewPage;
