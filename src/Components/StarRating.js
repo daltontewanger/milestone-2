1536121c58d70560fb9f9c2a3141cfe906a040f5
Emily Ochoa <eochoa156@gmail.com>
2024-05-12 03:53:46 -0700

    starRatikng componnent is finished

--- src/Components/StarRating.js
+++ src/Components/StarRating.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa"; //npm install react-icons
+import './StarRating.css';
 
 function StarRating() {
-    const [rating, setRating] = useState(null)
-    
+    const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
 
     return (
         <div className="starRating">
@@ -11,11 +12,21 @@ function StarRating() {
                 const currentRating = index + 1;
                 return(
                     <label>
-                        <input type="radio" name="starNumber" value={currentRating} onClick={() => setRating(currentRating)} />
-                        <FaStar size={30} />
+                        <input type="radio" 
+                        name="starNumber" 
+                        value={currentRating} 
+                        onClick={() => setRating(currentRating)} />
+                        <FaStar 
+                        className='star' 
+                        size={30} 
+                        color={currentRating <= (hover || rating) ? "gold" : "lightgrey"}
+                        onMouseEnter={() => setHover(currentRating)}
+                        onMouseLeave={() => setHover(null)}
+                        />
                     </label>
                 );
             })}
+            <p>Your rating is {rating}</p>
         </div>
     )
 }
